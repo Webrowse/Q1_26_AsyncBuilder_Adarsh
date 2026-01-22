9b3d3efa87f55ec9324791ba1dafc41902881a5d
Webrowse <great.adarsh@gmail.com>
2026-01-22 16:39:48 +0530

    Mint has successfully created and mint address print in console

--- solana-starter/ts/cluster1/spl_init.ts
+++ solana-starter/ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import wallet from "./turbine3-wallet.json"
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -13,6 +13,14 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
     try {
         // Start here
         // const mint = ???
+        const mint = await createMint(
+            connection, 
+            keypair, 
+            keypair.publicKey, 
+            null, 
+            6,
+        )
+        console.log("Mint Address: ", mint.toBase58());
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
