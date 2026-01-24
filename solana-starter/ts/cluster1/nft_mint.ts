3dbccd359c979494f614169bf2afff8aac28a4bc
Webrowse <great.adarsh@gmail.com>
2026-01-24 13:49:17 +0530

    Mint an NFT using createNft function

--- solana-starter/ts/cluster1/nft_mint.ts
+++ solana-starter/ts/cluster1/nft_mint.ts
@@ -2,7 +2,7 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../turbin3-wallet.json"
+import wallet from "./turbine3-wallet.json"
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -17,10 +17,19 @@ const mint = generateSigner(umi);
 
 (async () => {
     // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
+    const tx = await createNft(umi, {
+        mint,
+        sellerFeeBasisPoints: percentAmount(5.5),
+        name: 'PinkyRough',
+        symbol: "PINK",
+        isMutable: true,
+        uri: "https://gateway.lighthouse.storage/ipfs/bafkreiarxooaadxslwimhlsg7iudeepvpqtfp637aj7dlgqpugve5hce2u",
+    })
     
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
+
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
 })();
